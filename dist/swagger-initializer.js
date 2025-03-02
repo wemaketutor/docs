4faea31cf5fe1536d897f36d81a396f3d43fb4cf
MrEndor <mrendorer@gmail.com>
2025-03-02 12:15:23 +0300

    feat(swagger): add multiple files spec

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,14 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    urls: [
+      { url: "swagger/auth.yml", name: "Auth Spec" },
+      { url: "swagger/homeworks.yml", name: "Homeworks Spec" },
+      { url: "swagger/lessons.yml", name: "Lessons Spec" },
+      { url: "swagger/materials.yml", name: "Materials Spec" },
+      { url: "swagger/profile.yml", name: "Profile Spec" },
+      { url: "swagger/teachers.yml", name: "Teachers Spec" }
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
